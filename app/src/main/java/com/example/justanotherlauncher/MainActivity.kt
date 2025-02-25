395522853140ea8bfc49739f336e74b6728426e4
Glitch752 <xxGlitch752xx@gmail.com>
2025-02-25 00:13:12 -0600

    Style app list and add scrolling

--- app/src/main/java/com/example/justanotherlauncher/MainActivity.kt
+++ app/src/main/java/com/example/justanotherlauncher/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.justanotherlauncher
 
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
@@ -10,25 +9,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.drawablepainter.DrawablePainter
-
 
 class MainActivity : ComponentActivity() {
     private lateinit var appList: List<ResolveInfo>
@@ -60,7 +65,10 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun AppGridLayout(packageManager: PackageManager, apps: List<ResolveInfo>, startActivity: (Intent) -> Unit) {
-    FlowRow(modifier = Modifier.padding(12.dp)) {
+    FlowRow(
+        modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState()).absolutePadding(10.dp, 150.dp, 10.dp, 200.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
         apps.sortedWith(compareBy { app ->
             app.loadLabel(packageManager).toString()
         }).forEach { app ->
@@ -78,16 +86,21 @@ fun AppIcon(app: ResolveInfo, packageManager: PackageManager, startActivity: (In
                 addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             }
             startActivity(intent)
-        }
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Transparent, disabledContentColor = Color.Transparent),
+        contentPadding = PaddingValues(0.dp),
+        // Turn off corner rounding
+        shape = RectangleShape,
+        modifier = Modifier.width(80.dp).padding(4.dp)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.width(80.dp).padding(4.dp)
+            modifier = Modifier.fillMaxWidth()
         ) {
             Image(painter = DrawablePainter(app.loadIcon(packageManager)), contentDescription = "", modifier = Modifier.padding(6.dp).fillMaxWidth().aspectRatio(1f))
             Text(
                 text = app.loadLabel(packageManager).toString(),
-                modifier = Modifier.padding(0.dp, 2.dp, 0.dp, 0.dp).fillMaxWidth(),
+                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).fillMaxWidth(),
                 overflow = TextOverflow.Ellipsis,
                 color = Color.White,
                 fontSize = 11.sp,
